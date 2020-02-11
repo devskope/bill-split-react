f585fce543c8a5c8eaac37f8c3862d557e0f6cb4
devskope <ope.oguntoye@protonmail.com>
2020-02-11 12:36:07 +0100

    set up routing and global state provider [#3]

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,33 @@
-import React from "react";
-import "./App.css";
+import React, { useReducer } from "react";
+import { BrowserRouter as Router, Link } from "react-router-dom";
+import { RenderRoutes, routes } from "./router";
+import { Container, Menu } from "semantic-ui-react";
+import { initialState, stateContext, stateReducer } from "./store";
 
-const App = () => <p>React!</p>;
+const App = () => {
+  const [state, dispatch] = useReducer(stateReducer, initialState);
+
+  const navItems = [
+    { label: "Home", link: "/" },
+    { label: "About", link: "/about" }
+  ].map((item, idx) => (
+    <Menu.Item as={Link} key={idx} to={item.link}>
+      {item.label}
+    </Menu.Item>
+  ));
+
+  return (
+    <div>
+      <stateContext.Provider value={{ state, dispatch }}>
+        <Router>
+          <Container>
+            <Menu size="large" secondary pointing items={navItems} />
+          </Container>
+          <RenderRoutes routes={routes} />
+        </Router>
+      </stateContext.Provider>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/router/index.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+
+const RouteWithSubRoutes = route => (
+  <Route
+    path={route.path}
+    exact={route.exact}
+    render={props => (
+      <route.component
+        {...props}
+        {...route.componentProps}
+        routes={route.routes}
+      />
+    )}
+  />
+);
+
+export const RenderRoutes = ({ routes }) => (
+  <Switch>
+    {routes.map((route, i) => {
+      return <RouteWithSubRoutes key={route.key} {...route} />;
+    })}
+    <Route render={() => <h1>Requested resource not found!</h1>} />
+  </Switch>
+);
+
+export const routes = [
+  {
+    path: "/",
+    key: "home",
+    component: () => <h1>Home placeholder</h1>,
+    exact: true
+  },
+  {
+    path: "/about",
+    key: "about",
+    component: () => <h1>This is an about page</h1>,
+    exact: true
+  }
+];
